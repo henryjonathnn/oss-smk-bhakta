d56b44aac81725820bae76511671cf45a1a90a89
Henry Jonathan <henryjonathanchandra12@gmail.com>
2024-09-25 15:54:52 +0700

    add more features

--- resources/js/Layouts/Footer.jsx
+++ resources/js/Layouts/Footer.jsx
@@ -4,9 +4,9 @@ export const Footer = () => {
     return (
         <footer className="bg-gray-800 text-white py-8">
             <div className="container mx-auto px-4 md:px-8 lg:px-12">
-                <div className="flex flex-wrap justify-between">
+                <div className="flex flex-wrap justify-between mt-10">
                     <div className="w-full md:w-1/3 mb-6 md:mb-0">
-                        <h3 className="text-xl font-bold mb-4">OSIS SMA Bhakta</h3>
+                        <h3 className="text-xl font-bold mb-4">OSIS SMK Bhakti Wiayata Kediri</h3>
                         <p>Jl. Pendidikan No. 123, Kota Anda, 12345</p>
                     </div>
                     <div className="w-full md:w-1/3 mb-6 md:mb-0">
@@ -24,7 +24,7 @@ export const Footer = () => {
                     </div>
                 </div>
                 <div className="border-t border-gray-700 mt-8 pt-8 text-center">
-                    <p>&copy; 2024 OSIS SMA Bhakta. Hak Cipta Dilindungi.</p>
+                    <p className='underline'>&copy; 2024 OSIS SMK Bhakti Wiyata. Hak Cipta Dilindungi.</p>
                 </div>
             </div>
         </footer>
--- resources/js/Layouts/Navbar.jsx
+++ resources/js/Layouts/Navbar.jsx
@@ -1,35 +1,45 @@
 import React from 'react'
+import { Link, usePage } from '@inertiajs/inertia-react'
 
-const NavItem = ({ href, text }) => (
-  <a
-    href={href}
-    className="text-gray-700 hover:bg-gray-200 hover:text-slate-950 px-2 py-2 rounded-md duration-500"
-  >
-    {text}
-  </a>
-)
+const NavItem = ({ href, text }) => {
+  const { url } = usePage()
+  const isActive = url.startsWith(href)
+
+  return (
+    <Link
+      href={href}
+      className={`px-3 py-2 rounded-md text-sm font-bold ${
+        isActive
+          ? 'bg-slate-900 text-white'
+          : 'text-gray-900 hover:bg-gray-100 hover:text-gray-800'
+      }`}
+    >
+      {text}
+    </Link>
+  )
+}
 
 const navItems = [
-  { href: "#", text: "Home" },
-  { href: "#", text: "Sekbid" },
-  { href: "#", text: "Event" },
-  { href: "#", text: "Artikel" },
-  { href: "#", text: "Kotak Saran" }
+  { href: "/", text: "Home" },
+  { href: "/struktural", text: "Struktural" },
+  { href: "/event", text: "Event" },
+  { href: "/artikel", text: "Artikel" },
+  { href: "/kotak-saran", text: "Kotak Saran" }
 ]
 
 export const Navbar = () => {
   return (
-    <nav className="bg-white">
-      <div className="max-w-7xl mx-auto px-4 md:px-8 lg:px-12">
+    <nav className="bg-white py-2 sticky top-0 z-50 shadow-lg">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
-          <div className="flex items-center bg-white">
+          <div className="flex items-center">
             <div className="flex-shrink-0">
-              <img className="h-28 w-28" src="/assets/logo.png" alt="Workflow" />
+              <img className="h-28 w-auto" src="/assets/logo.png" alt="OSS Bhakta" />
             </div>
-            <h1 className="text-xl font-bold text-slate-800">OSS Bhakta</h1>
+            <h1 className="ml-2 text-xl font-bold text-gray-800">OSS Bhakta</h1>
           </div>
           <div className="hidden md:block">
-            <div className="ml-10 flex items-baseline space-x-3 text-base font-bold">
+            <div className="ml-10 flex items-baseline space-x-4">
               {navItems.map((item, index) => (
                 <NavItem key={index} href={item.href} text={item.text} />
               ))}
