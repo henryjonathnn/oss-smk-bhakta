3bdc0e307364943552767da457948e593a244cd0
Henry Jonathan <henryjonathanchandra12@gmail.com>
2024-09-30 23:32:54 +0700

    add hero in event page and struktural page

--- resources/js/Pages/Client/Event/Index.jsx
+++ resources/js/Pages/Client/Event/Index.jsx
@@ -3,6 +3,7 @@ import { Head } from '@inertiajs/react';
 import ClientLayout from '../../../Components/templates/ClientLayouts';
 import { StrukturalCard, CenteredCardContainer } from '../../../Components/molecules/StrukturalCard';
 import EventCard from '../../../Components/molecules/EventCard';
+import Hero from '../Home/Hero';
 
 const events = [
   { title: "MPLS Bhakta 2023", date: "15 Juli 2023", image: "/assets/img/event/mpls2023.jpg" },
@@ -14,11 +15,17 @@ const EventPage = () => {
   return (
     <ClientLayout>
       <Head title="Struktural OSIS" />
+      <Hero
+        title="Akan segera digelar event MOBA 4 2024"
+        subtitle="Meaning of Bhakta"
+        buttonText="Jelajahi Lebih Lanjut"
+        backgroundImage="/assets/img/event/moba3.jpg"
+      />
       <div className="container mx-auto px-4 md:px-8 lg:px-12 mb-20 mt-20">
         <h1 className="text-4xl font-bold text-center mb-12 text-indigo-900">Struktural OSIS</h1>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
           {events.map((event, index) => (
-            <EventCard
+            <EventCard className="transform transition duration-500 hover:scale-105 shadow-lg"
               key={index}
               title={event.title}
               date={event.date}
--- resources/js/Pages/Client/Struktural/Index.jsx
+++ resources/js/Pages/Client/Struktural/Index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Head } from '@inertiajs/react';
 import ClientLayout from '../../../Components/templates/ClientLayouts';
 import { StrukturalCard, CenteredCardContainer } from '../../../Components/molecules/StrukturalCard';
+import Hero from '../Home/Hero';
 
 const strukturalData = [
   {
@@ -31,6 +32,12 @@ const StrukturalPage = () => {
   return (
     <ClientLayout>
       <Head title="Struktural OSIS" />
+      <Hero
+        title="Akan segera digelar event MOBA 4 2024"
+        subtitle="Meaning of Bhakta"
+        buttonText="Jelajahi Lebih Lanjut"
+        backgroundImage="/assets/img/event/moba3.jpg"
+      />
       <div className="container mx-auto px-4 py-12 mb-10 mt-10">
         <h1 className="text-4xl font-bold text-center mb-12 text-indigo-900">Struktural OSIS</h1>
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8 justify-items-center">
